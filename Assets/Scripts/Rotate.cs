0b3f4ede4482f59aced1abd9c5d7d08cd06c052e
dragonfyre23 <blakelowe20@gmail.com>
2018-11-12 22:14:16 -0500

    Added autorotate script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public GameObject rotor;
+    public float rotationSpeed;
+
+    private Transform t;
+    // Use this for initialization
+    void Start () {
+        t = rotor.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currentPosition = t.localEulerAngles;
+        t.localEulerAngles = new Vector3(0, currentPosition.y + rotationSpeed * Time.deltaTime, 0);
+    }
+}
