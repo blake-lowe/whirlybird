b6377785ae0de1ee758c311c4d44854f90866a75
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2018-11-14 13:08:10 -0500

    Added chopper model rotation control script

---
+++ Assets/Scripts/RotationControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationControl : MonoBehaviour {
+
+    public Vector3 targetRotation;
+    Transform t;
+	// Use this for initialization
+	void Start () {
+        t = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.eulerAngles = targetRotation;//for now need to add follow parent y and z rotation. Or use a second script placed on the parent
+	}
+}
