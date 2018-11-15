492970f577eb3d241c87b5968be4afc575c66bd3
dragonfyre23 <blakelowe20@gmail.com>
2018-11-14 23:32:00 -0500

    Created PivotController and added variables

---
+++ Assets/Scripts/PivotController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotController : MonoBehaviour {//unfinished
+
+    public Rigidbody HingeY;
+    public float YTorqueFactor;
+    public Rigidbody HingeZ;
+    public float ZTorqueFactor;
+    public Transform Chopper;
+    public float ChopperRotationLimit;//in one direction
+	// Use this for initialization
+	void Start () {
+		//todo
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//todo
+	}
+}
