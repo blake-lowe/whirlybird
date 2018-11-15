cc1b66a742080134e4331d93e4b13d0f15f0abba
dragonfyre23 <blakelowe20@gmail.com>
2018-11-14 23:29:19 -0500

    Created and implemented DeactivateAfterTime.cs

---
+++ Assets/Scripts/DeactivateAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAfterTime : MonoBehaviour {//finished
+
+    public float deactivateAfterSeconds;
+	// Use this for initialization
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(deactivateAfterSeconds);
+        gameObject.SetActive(false);
+    }
+	void Start ()
+    {
+        StartCoroutine(Wait());
+    }
+}
