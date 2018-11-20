f5c5522b11f1b80c265dd8ac4736ee2c4ae23c49
dragonfyre23 <blakelowe20@gmail.com>
2018-11-19 22:17:40 -0500

    Created and fully implemented LandingPadScript
    State system and colliders to work with GameLogic

---
+++ Assets/Scripts/LandingPadScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingPadScript : MonoBehaviour {//put this script on the trigger object
+
+    public Material active;
+    public Material inactive;
+    MeshRenderer mr;
+    public bool isActive;
+    public bool isTriggered;
+
+	// Use this for initialization
+	void Awake () {
+        mr = gameObject.GetComponent<MeshRenderer>();
+        mr.material = inactive;
+        isActive = false;
+        isTriggered = false;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActive)
+        {
+            isTriggered = true;
+        }
+    }
+    public void Activate()
+    {
+        mr.material = active;
+        isActive = true;
+    }
+    public void Deactivate()
+    {
+        mr.material = inactive;
+        isActive = false;
+        isTriggered = false;
+    }
+}
