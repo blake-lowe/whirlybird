c064932c15a9df7ca5b6781a4ea768afd282f52c
dragonfyre23 <blakelowe20@gmail.com>
2018-11-21 12:06:30 -0500

    Added Main Menu scene and loading system

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public string SceneToLoad;
+    public Button Button1;
+
+    void Start()
+    {
+        Button1.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
